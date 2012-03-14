d44bf3dcd64c385a6cc8007565af550c7f817f51
redcoffee <hongcoffee@gmail.com>
2012-03-14 14:16:41 +0900

    Revert "Revert "first setup""
    This reverts commit 3190fba57c0bc3a2e1781ccf6d8b522b52cb9786.

---
+++ feed/menu.php
@@ -0,0 +1,25 @@
+<section class="side left_wrap pull-left">
+	<h2 class="profile">
+		<a href=""><img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/48655_501842750_7054_q.jpg"></a>
+		<div class="inner_wrap"><a href="" class="name">홍커피</a></div>
+	</h2>
+	<ul class="nav nav-list">
+		<li><a href=""><i class="icon-check"></i>알림<span class="badge">2</span></a></li>
+		<li class="nav-header">즐겨찾기 ★</li>
+		<li class="active"><a href=""><i class="icon-inbox"></i>모아보기</a></li>
+		<li><a href=""><i class="icon-user"></i>내 이웃<span class="badge focus">5</span></a></li>
+		<li><a href=""><i class="icon-leaf"></i>모임<span class="badge">2</span></a></li>	
+		<li><a href=""><i class="icon-tag"></i>할인쿠폰<span class="badge">4</span></a></li>
+		<li><a href=""><i class="icon-map-marker"></i>매장소식<span class="badge">6</span></a></li>
+		<li><a href=""><i class="icon-comment"></i>애완동물<span class="badge">1</span></a></li>
+		<li class="nav-header">분류</li>		
+		<li><a href=""><i class="icon-headphones"></i>중고물품</a></li>
+		<li><a href=""><i class="icon-user"></i>구인구직<span class="badge">12</span></a></li>
+		<li><a href=""><i class="icon-home"></i>부동산<span class="badge">3</span></a></li>
+		<li><a href=""><i class="icon-list-alt"></i>지역뉴스<span class="badge">1</span></a></li>
+		<li class="nav-header">토픽 #</li>
+		<li><a href=""><i class="icon-comment"></i>다이어트</a></li>
+		<li><a href=""><i class="icon-comment"></i>싸이클<span class="badge">3</span></a></li>
+		<li><a href=""><i class="icon-comment"></i>예방접종<span class="badge">1</span></a></li>		
+	</ul>		
+</section>
---
+++ place/all.php
@@ -0,0 +1,173 @@
+<section class="content_wrap place pull-left">	
+	
+	<ul class="photo-album">
+		<li><a href=""><img src="https://img-s.foursquare.com/derived_pix/SVQ0PC2KJGFGPN3MDIVO3EIK2DLC33JXEOLG22JXFLQWPFRJ_300x300.jpg"></a></li>
+		<li><a href=""><img src="https://img-s.foursquare.com/derived_pix/2BEYWYMKK15NTECMEZDMKX4AGHPEPXIY2Z2KDZNS0G1OW5UQ_300x300.jpg"></a></li>
+		<li><a href=""><img src="https://img-s.foursquare.com/derived_pix/SAV0W2ZMO2HMZDTDRW3XTDZ5MNYNEIF3EZ02CHSQWLZZUQXD_300x300.jpg"></a></li>
+		<li><a href=""><img src="https://img-s.foursquare.com/derived_pix/B4OWISRE2X2A3URNNIPZ2G4CSBJTCD2B0CGVICPDFHD4W4VX_300x300.jpg"></a></li>
+		<div class="clear"></div>
+	</ul>			
+	
+	<div class="composer">
+		<ul class="menu nav nav-pills">
+			<li class="active"><a href="#message" data-toggle="tab"><i class="icon-comment"></i>메시지</a></li>
+			<li><a href="#blog" data-toggle="tab"><i class="icon-file"></i>블로그리뷰</a></li>
+			<li><a href="#question" data-toggle="tab"><i class="icon-question-sign"></i>질문</a></li>
+		</ul>
+		<div class="tab-content">				
+			<div class="tab-pane active" id="message">					
+				<textarea class="input" placeholder="담벼락에 메시지를 남겨주세요."></textarea>
+				<div class="control_wrap">
+					<a class="btn btn-success pull-right" href="#"><b>남기기</b></a>
+					<ul class="meta nav nav-pills">							
+						<li><a href="#"><i class="icon-picture"></i>사진</a></li>
+						<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-star"></i>평가 <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="">★★★★★</a></li>
+								<li><a href="">★★★★☆</a></li>
+								<li><a href="">★★★☆☆</a></li>
+								<li><a href="">★★☆☆☆</a></li>
+								<li><a href="">★☆☆☆☆</a></li>
+							</ul>
+						</li>
+					</ul>						
+				</div>
+			</div>
+			<div class="tab-pane" id="blog">
+				블로그리뷰  인터페이스
+			</div>			
+			<div class="tab-pane" id="question">
+				질문 인터페이스
+			</div>
+		</div>						
+	</div>
+			
+	<ul class="timeline">
+		<li>
+			<a href="" class="profile"><img src="./img/store_2.jpg"></a>
+			<div class="inner_wrap">							
+				<h4>
+					<a href="" class="name"><i class="icon-comment"></i>아비꼬</a>
+				</h4>
+				<p class="message">
+					오늘 점심으로 매콤한 3단계 새우튀김 카레를 도전해보시는 것은 어떻세요?
+				</p>
+				<p class="photo">
+					<img src="./img/pic_3.jpg">
+				</p>				
+				<div class="meta">
+					<a href="">좋아요</a> · <a href="">댓글달기</a> · <a href="">잠원동</a>에서 · <span>1시간전</span> 
+				</div>
+				<ul class="reply">								
+					<li>
+						<a href="" class="profile"><img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/371087_100000541762691_668830605_q.jpg"></a>
+						<div class="inner_wrap">
+							<p class="message"><a href="" class="name">정승화</a> 3단계는 너무 매워서 못먹겠던데;;</p>
+							<div class="meta">
+								<span>30분전</span> · <a href="">좋아요</a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<a href="" class="profile"><img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/41647_564625039_5764_q.jpg"></a>
+						<div class="inner_wrap">
+							<p class="message"><a href="" class="name">김진중</a> 사실 3단계는 되어야 카레죠!</p>
+							<div class="meta">
+								<span>25분전</span> · <a href="">좋아요</a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<a href="" class="profile"><img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/70772_1124883057_1942447_q.jpg"></a>
+						<div class="inner_wrap">
+							<p class="message"><a href="" class="name">최미나</a> 일식 카레라고 말하기에는 좀 부족한 느낌.퓨전 카레집으로서는 괜찮은 편~ㅋ</p>
+							<div class="meta">
+								<span>10분전</span> · <a href="">좋아요</a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<a href="" class="profile"><img src="https://www.yammer.com/yamage/photos/5991091/_-_____bigger_small.jpg"></a>
+						<div class="inner_wrap">
+							<p class="message"><a href="" class="name">박영욱</a> 안그래도 오늘 카레 쫌 땡겼는 데, 카레나 먹을까!</p>
+							<div class="meta">
+								<span>10분전</span> · <a href="">좋아요</a>
+							</div>
+						</div>
+					</li>					
+					<li class="input">
+						<form><input type="text" placeholder="댓글을 남겨주세요."></form>
+					</li>								
+				</ul>
+			</div>
+		</li>
+		<li>
+			<a href="" class="profile"><img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/260748_100002538901541_1971821910_q.jpg"></a>
+			<div class="inner_wrap">							
+				<h4>
+					<a href="" class="name"><i class="icon-comment"></i>한소</a>
+				</h4>
+				<p class="message">
+					아비꼬는 매장별 맛 편차가 없는 듯 ㅎ 아기맛은 넘 심심하고.. 매운거 아예 못드시는 분들 아니시라면 3단계 쯤 드셔야 화끈하죠!ㅎ
+				</p>
+				<div class="meta">
+					<a href="">좋아요</a> · <a href="">댓글달기</a> · <a href="">잠원동</a>에서 · <span>5분전</span> 
+				</div>
+			</div>
+		</li>					
+		<li>
+			<a href="" class="profile"><img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/273431_1346281873_2170807_q.jpg"></a>
+			<div class="inner_wrap">							
+				<h4>
+					<a href="" class="name"><i class="icon-comment"></i>우성섭</a><span class="grade">★★★★☆</span>
+				</h4>
+				<p class="message">
+					요새는 어딜가나 볼수 있는 아비꼬 카레! 깔끔하면서도 진한 카레가 정말 좋네요. 밥과 카레 리필되니깐 양의 걱정은 없습니다ㅋ 프렌차이즈화에 성공했다고 할수있을정도로 어디 매장이든지 맛이 통일한게 최고 장점인것 같습니다^^ 
+				</p>
+				<p class="photo">
+					<img src="http://cfile2.uf.tistory.com/image/194452214C7B8D295DE129">
+				</p>
+				<div class="meta">
+					<a href="">좋아요</a> · <a href="">댓글달기</a> · <a href="">잠원동</a>에서 · <span>10분전</span> 
+				</div>
+				<ul class="reply">								
+					<li>
+						<a href="" class="profile"><img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/195283_100000857891713_6808756_q.jpg"></a>
+						<div class="inner_wrap">
+							<p class="message"><a href="" class="name">장효진</a> 맛있겠네요;;</p>
+							<div class="meta">
+								<span>1분전</span> · <a href="">좋아요</a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<a href="" class="profile"><img src="https://www.yammer.com/yamage/photos/11058466/z_small.jpg"></a>
+						<div class="inner_wrap">
+							<p class="message"><a href="" class="name">박미경</a> 오늘 유독 딱땡기네요!!</p>
+							<div class="meta">
+								<span>1분전</span> · <a href="">좋아요</a>
+							</div>
+						</div>
+					</li>
+					<li class="input">
+						<form><input type="text" placeholder="댓글을 남겨주세요."></form>
+					</li>								
+				</ul>							
+			</div>
+		</li>
+		<li>
+			<a href="" class="profile"><img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/161771_100000738621132_357787150_q.jpg"></a>
+			<div class="inner_wrap">							
+				<h4>
+					<a href="" class="name"><i class="icon-comment"></i>신현이</a><span class="grade">★★★☆☆</span>
+				</h4>
+				<p class="message">
+					각자 기호에 맞게 주문을 할 수있어 선택의 폭(?)이 넓은 집.맛과 양이 비례하는 집이다.근데 이것저것 적당히 토핑하면 밥값은 예상범위를 벗어난다.
+				</p>
+				<div class="meta">
+					<a href="">좋아요</a> · <a href="">댓글달기</a> · <a href="">잠원동</a>에서 · <span>14분전</span> 
+				</div>
+			</div>
+		</li>
+	</ul>		
+</section>
